d19052b280b8401ff6dcaa995dd22f8c400165d2
samlar10 <82476316+samlar10@users.noreply.github.com>
2022-04-04 12:32:53 +1200

    4/04

--- Godot Assets/Code/AI_DrivingCode.gd
+++ Godot Assets/Code/AI_DrivingCode.gd
@@ -1,13 +1,9 @@
 extends KinematicBody
 
-export var gravity = -20.0
-export var wheel_base = 0.6
-export var steering_limit = 10.0
-export var engine_power = 6.0
-export var braking = -9.0
-export var friction = -2.0
-export var drag = -2.0
-export var max_speed_reverse = 3.0
+
+export var max_speed = 350
+export var steer_force = 0.1
+export var look_ahead = 100
 export var num_rays = 16
 
 var ray_directions = {}
@@ -21,9 +17,36 @@ var velocity = Vector3.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	interest.resize(num_rays)
+	danger.resize(num_rays)
+	ray_directions.resize(num_rays)
+	for i in num_rays:
+		var angle = i * 2 * PI / num_rays
+		ray_directions[i] = Vector3.RIGHT.rotated(angle)
+		 
+func _physics_process(delta):
+	set_interest()
+	set_danger()
+	choose_direction()
+	var desired_velocity = chosen_dir.rotated(rotation) * max_speed
+	velocity = velocity.linear_interpolate(desired_velocity, steer_force)
+	rotation = velocity.angle()
+	move_and_collide(velocity * delta)
+	
+func set_interest():
+	if owner and owner.has_method("get_path_direction"):
+		var path_direction = owner.get_path_direction(position)
+		for i in num_rays:
+			var d = ray_directions[i].rotated(rotation).dot(path_direction)
+			interst[i] = max(0, d)
+	else:
+		set_default_interest()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func set_default_interest():
+	for i in num_rays:
+		var d = ray_directions[i].rotated(rotation).dot(transform.x)
+		interet[i] = max(0, d)
+		
+func set_danger():
+	var space_state = get_world_3d().direct_space_state

d82f04179ad3e9304997c8812d2e06da6a108394
samlar10 <82476316+samlar10@users.noreply.github.com>
2022-05-04 11:39:29 +1200

    4/05
    highlands track is added

--- Godot Assets/Code/TestWorld.gd
+++ Godot Assets/Code/TestWorld.gd
@@ -1,9 +1,14 @@
 extends Spatial
-
+#
 func get_path_direction(position):
-	var offset = $Basic_track/Path.curve.get_closest_offset(position)
-	$Basic_track/Path/PathFollow.offset = offset
-	return $Basic_track/Path/PathFollow.transform.basis.z
-	
+#	var offset = $Basic_track/Path.curve.get_closest_offset(position)
+#	$Basic_track/Path/PathFollow.offset = offset
+#	return $Basic_track/Path/PathFollow.transform.basis.z
+#
+	var offset = $Highlands_track2/Path.curve.get_closest_offset(position)
+	$Highlands_track2/Path/PathFollow.offset = offset
+	return $Highlands_track2/Path/PathFollow.transform.basis.z
+
 func _ready():
-	print($Basic_track["transform"])
+#	print($Basic_track["transform"])
+	print($Highlands_track2["transform"])

fa50f62fc3381350aee5079501c3b9ea551c2d79
samlar10 <82476316+samlar10@users.noreply.github.com>
2022-05-12 11:30:21 +1200

    12/05

--- Godot Assets/Code/TestWorld.gd
+++ Godot Assets/Code/TestWorld.gd
@@ -5,10 +5,10 @@ func get_path_direction(position):
 #	$Basic_track/Path/PathFollow.offset = offset
 #	return $Basic_track/Path/PathFollow.transform.basis.z
 #
-	var offset = $Highlands_track2/Path.curve.get_closest_offset(position)
-	$Highlands_track2/Path/PathFollow.offset = offset
-	return $Highlands_track2/Path/PathFollow.transform.basis.z
+	var offset = $Highlands_track_walls/Path.curve.get_closest_offset(position)
+	$Highlands_track_walls/Path/PathFollow.offset = offset
+	return $Highlands_track_walls/Path/PathFollow.transform.basis.z
 
 func _ready():
 #	print($Basic_track["transform"])
-	print($Highlands_track2["transform"])
+	print($Highlands_track_walls["transform"])
